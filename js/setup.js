e3e76c7a7dbb7d0e4a7f8a504a329171420f7202
Deilan <deilan@mail.ru>
2017-12-04 15:43:57 +0300

    Refactoring

--- js/setup.js
+++ js/setup.js
@@ -51,8 +51,7 @@
   var setupEl = document.querySelector('.setup');
   var setupOpenEl = document.querySelector('.setup-open');
   var setupCloseEl = setupEl.querySelector('.setup-close');
-  setupOpenEl.addEventListener('click', onSetupOpenClick);
-  setupOpenEl.addEventListener('keydown', onSetupOpenKeydown);
+
 
   var setupUserNameEl = setupEl.querySelector('.setup-user-name');
   var setupUserNameInput = setupEl.querySelector('.setup-user-name');
@@ -61,20 +60,88 @@
   var setupWizardCoatEl = setupPlayerEl.querySelector('.wizard-coat');
   var setupWizardEyesEl = setupPlayerEl.querySelector('.wizard-eyes');
   var setupFireballEl = setupPlayerEl.querySelector('.setup-fireball-wrap');
-  setupWizardCoatEl.addEventListener('click', onSetupWizardCoatClick);
-  setupWizardEyesEl.addEventListener('click', onSetupWizardEyesClick);
-  setupFireballEl.addEventListener('click', onSetupFireballClick);
 
-  function onSetupWizardCoatClick(evt) {
-    setRandomStyleProp(evt.target, 'fill', COAT_COLORS);
+  initSetupHandlers({
+    root: document,
+    setup: setupEl,
+    setupOpen: setupOpenEl,
+    setupClose: setupCloseEl
+  });
+  initCustomizationHandlers({
+    setupWizardCoat: setupWizardCoatEl,
+    setupWizardEyes: setupWizardEyesEl,
+    setupFireball: setupFireballEl
+  });
+
+  function initSetupHandlers(els) {
+    els.setupOpen.addEventListener('click', onSetupOpenClick);
+    els.setupOpen.addEventListener('keydown', onSetupOpenKeydown);
+
+    function openSetup() {
+      els.setup.classList.remove('hidden');
+      els.setupOpen.removeEventListener('click', onSetupOpenClick);
+      els.setupOpen.removeEventListener('keydown', onSetupOpenKeydown);
+      els.setupClose.addEventListener('click', onSetupCloseClick);
+      els.setupClose.addEventListener('keydown', onSetupCloseKeydown);
+      els.root.addEventListener('keydown', onEscKeydown);
+    }
+
+    function closeSetup() {
+      els.setup.classList.add('hidden');
+      els.setupClose.removeEventListener('click', onSetupCloseClick);
+      els.setupClose.removeEventListener('keydown', onSetupCloseKeydown);
+      els.setupOpen.addEventListener('click', onSetupOpenClick);
+      els.setupOpen.addEventListener('keydown', onSetupOpenKeydown);
+      els.root.removeEventListener('keydown', onEscKeydown);
+    }
+
+    function onSetupOpenClick() {
+      openSetup();
+    }
+
+    function onSetupOpenKeydown(evt) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        openSetup();
+      }
+    }
+
+    function onSetupCloseClick() {
+      closeSetup();
+    }
+
+    function onEscKeydown(evt) {
+      // Если фокус находится на форме ввода имени, то окно закрываться не должно
+      if (evt.target === setupUserNameEl) {
+        return;
+      }
+      if (evt.keyCode === ESC_KEYCODE) {
+        closeSetup();
+      }
+    }
+
+    function onSetupCloseKeydown(evt) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        closeSetup();
+      }
+    }
   }
 
-  function onSetupWizardEyesClick(evt) {
-    setRandomStyleProp(evt.target, 'fill', EYES_COLORS);
-  }
+  function initCustomizationHandlers(els) {
+    els.setupWizardCoat.addEventListener('click', onSetupWizardCoatClick);
+    els.setupWizardEyes.addEventListener('click', onSetupWizardEyesClick);
+    els.setupFireball.addEventListener('click', onSetupFireballClick);
 
-  function onSetupFireballClick(evt) {
-    setRandomStyleProp(evt.target, 'backgroundColor', FIREBALL_COLORS);
+    function onSetupWizardCoatClick(evt) {
+      setRandomStyleProp(evt.target, 'fill', COAT_COLORS);
+    }
+
+    function onSetupWizardEyesClick(evt) {
+      setRandomStyleProp(evt.target, 'fill', EYES_COLORS);
+    }
+
+    function onSetupFireballClick(evt) {
+      setRandomStyleProp(evt.target, 'backgroundColor', FIREBALL_COLORS);
+    }
   }
 
   function setRandomStyleProp(target, prop, values) {
@@ -94,55 +161,6 @@
       evt.target.setCustomValidity('Имя обязательно для заполнения');
     }
   });
-
-
-  function openSetup() {
-    setupEl.classList.remove('hidden');
-    setupOpenEl.removeEventListener('click', onSetupOpenClick);
-    setupOpenEl.removeEventListener('keydown', onSetupOpenKeydown);
-    setupCloseEl.addEventListener('click', onSetupCloseClick);
-    setupCloseEl.addEventListener('keydown', onSetupCloseKeydown);
-    document.addEventListener('keydown', onEscKeydown);
-  }
-
-  function closeSetup() {
-    setupEl.classList.add('hidden');
-    setupCloseEl.removeEventListener('click', onSetupCloseClick);
-    setupCloseEl.removeEventListener('keydown', onSetupCloseKeydown);
-    document.removeEventListener('keydown', onEscKeydown);
-    setupOpenEl.addEventListener('click', onSetupOpenClick);
-    setupOpenEl.addEventListener('keydown', onSetupOpenKeydown);
-  }
-
-  function onSetupOpenClick() {
-    openSetup();
-  }
-
-  function onSetupOpenKeydown(evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
-      openSetup();
-    }
-  }
-
-  function onSetupCloseClick() {
-    closeSetup();
-  }
-
-  function onEscKeydown(evt) {
-    // Если фокус находится на форме ввода имени, то окно закрываться не должно
-    if (evt.target === setupUserNameEl) {
-      return;
-    }
-    if (evt.keyCode === ESC_KEYCODE) {
-      closeSetup();
-    }
-  }
-
-  function onSetupCloseKeydown(evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
-      closeSetup();
-    }
-  }
 
   renderSetup(getWizards(WIZARDS_COUNT, {
     firstNames: FIRST_NAMES,
