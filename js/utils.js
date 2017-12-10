6344f852c1a3467273113320935271a7db05d33c
Deilan <deilan@mail.ru>
2017-12-10 13:42:57 +0300

    Reworked setRandomStyleProp

--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,29 @@
 'use strict';
 
 (function () {
+  window.getRandomIntBetween = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
   window.getRandomElement = function (arr) {
     var index = Math.floor(Math.random() * (arr.length - 1));
     return arr[index];
   };
 
+  window.pullRandomElement = function (arr) {
+    var index = window.getRandomIntBetween(0, arr.length - 1);
+    return arr.splice(index, 1)[0];
+  };
+
   window.setRandomStyleProp = function (target, prop, values) {
-    var value = target.style[prop];
-    while (value === target.style[prop]) {
-      value = window.getRandomElement(values);
+    values = values.slice();
+    for (var i = 0; i < values.length; i++) {
+      var value = window.pullRandomElement(values);
+      if (value !== target.style[prop]) {
+        break;
+      }
     }
     target.style[prop] = value;
   };
