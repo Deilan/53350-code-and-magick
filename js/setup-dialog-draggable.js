a0e722aef59fae68482a2d358531cfa0bd791e83
Deilan <deilan@mail.ru>
2017-12-16 16:47:39 +0300

    Review's suggestions for setup-dialog-draggable.js

--- js/setup-dialog-draggable.js
+++ js/setup-dialog-draggable.js
@@ -2,61 +2,75 @@
 
 (function () {
   window.initSetupDialogDraggable = function (els) {
+    var dragEnv = {
+      els: els,
+      initialCoords: null,
+      shift: null
+    };
+    var onMouseDown = onMouseDownFactory(dragEnv);
     els.upload.addEventListener('mousedown', onMouseDown);
+  };
 
-    function onMouseDown(evt) {
+  function onMouseDownFactory(dragEnv) {
+    return function (evt) {
       evt.preventDefault();
 
-      var initialCoords = {
+      dragEnv.initialCoords = {
         x: evt.clientX,
         y: evt.clientY
       };
-
+      var onMouseMove = onMouseMoveFactory(dragEnv);
+      var onMouseUp = onMouseUpFactory(onMouseMove);
       document.addEventListener('mousemove', onMouseMove);
       document.addEventListener('mouseup', onMouseUp);
+    };
+  }
 
-      function onMouseMove(moveEvt) {
-        moveEvt.preventDefault();
+  function onMouseMoveFactory(dragEnv) {
+    return function (evt) {
+      evt.preventDefault();
 
-        var shift = {
-          x: initialCoords.x - moveEvt.clientX,
-          y: initialCoords.y - moveEvt.clientY
-        };
+      dragEnv.shift = {
+        x: dragEnv.initialCoords.x - evt.clientX,
+        y: dragEnv.initialCoords.y - evt.clientY
+      };
 
-        initialCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
-        var targetCoords = getTargetCoords(shift);
-        els.setup.style.left = targetCoords.x + 'px';
-        els.setup.style.top = targetCoords.y + 'px';
-      }
+      dragEnv.initialCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+      var targetCoords = getTargetCoords(dragEnv);
+      dragEnv.els.setup.style.left = targetCoords.x + 'px';
+      dragEnv.els.setup.style.top = targetCoords.y + 'px';
+    };
+  }
 
-      function getTargetCoords(shift) {
-        var setupRect = els.setup.getBoundingClientRect();
-        var setupParentRect = els.setup.parentElement.getBoundingClientRect();
-        var targetCoords = {
-          x: els.setup.offsetLeft - shift.x,
-          y: els.setup.offsetTop - shift.y
-        };
-        // fix coordinates if they're out of bounds
-        if (targetCoords.x < setupRect.width / 2) {
-          targetCoords.x = setupRect.width / 2;
-        } else if (targetCoords.x + setupRect.width / 2 > setupParentRect.width) {
-          targetCoords.x = setupParentRect.width - setupRect.width / 2;
-        }
+  function onMouseUpFactory(onMouseMove) {
+    var onMouseUp = function (evt) {
+      evt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    return onMouseUp;
+  }
 
-        if (targetCoords.y < setupParentRect.y) {
-          targetCoords.y = setupParentRect.y;
-        } // allow getting out of bound at the bottom
-        return targetCoords;
-      }
-
-      function onMouseUp(upEvt) {
-        upEvt.preventDefault();
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-      }
+  function getTargetCoords(dragEnv) {
+    var setupRect = dragEnv.els.setup.getBoundingClientRect();
+    var setupParentRect = dragEnv.els.setup.parentElement.getBoundingClientRect();
+    var targetCoords = {
+      x: dragEnv.els.setup.offsetLeft - dragEnv.shift.x,
+      y: dragEnv.els.setup.offsetTop - dragEnv.shift.y
+    };
+    // fix coordinates if they're out of bounds
+    if (targetCoords.x < setupRect.width / 2) {
+      targetCoords.x = setupRect.width / 2;
+    } else if (targetCoords.x + setupRect.width / 2 > setupParentRect.width) {
+      targetCoords.x = setupParentRect.width - setupRect.width / 2;
     }
-  };
+
+    if (targetCoords.y < setupParentRect.y) {
+      targetCoords.y = setupParentRect.y;
+    } // allow getting out of bound at the bottom
+    return targetCoords;
+  }
 })();
