265e88978f04b006bacef08354ac301dfb5f34d6
Deilan <deilan@mail.ru>
2017-12-16 16:47:29 +0300

    Review's suggestions for setup-artifacts-draggable.js

--- js/setup-artifacts-draggable.js
+++ js/setup-artifacts-draggable.js
@@ -2,64 +2,81 @@
 
 (function () {
   window.initSetupArtifactsDraggable = function (els) {
-    var draggableEl;
     var cells = els.setupArtifacts.querySelectorAll('.setup-artifacts-cell');
+    var dragEls = {
+      cells: cells
+    };
+    var onDragStart = onDragStartFactory(dragEls);
+    var onDragEnd = onDragEndFactory(dragEls);
+    var onDrop = onDropFactory(dragEls);
     els.setupArtifactsShop.addEventListener('dragstart', onDragStart);
     els.setupArtifactsShop.addEventListener('dragend', onDragEnd);
     els.setupArtifacts.addEventListener('dragstart', onDragStart);
     els.setupArtifacts.addEventListener('dragend', onDragEnd);
 
-    function onDragStart(evt) {
-      draggableEl = evt.target;
+    els.setupArtifacts.addEventListener('dragover', onDragOver);
 
-      for (var i = 0; i < cells.length; i++) {
+    els.setupArtifacts.addEventListener('drop', onDrop);
+
+    els.setupArtifacts.addEventListener('dragenter', onDragEnter);
+
+    els.setupArtifacts.addEventListener('dragleave', onDragLeave);
+  };
+
+  function onDragStartFactory(els) {
+    return function (evt) {
+      els.draggable = evt.target;
+      for (var i = 0; i < els.cells.length; i++) {
         // highlight a cell only if it's empty
-        if (!cells[i].hasChildNodes()) {
-          cells[i].style.outline = '2px dashed red';
+        if (!els.cells[i].hasChildNodes()) {
+          els.cells[i].style.outline = '2px dashed red';
         }
       }
-    }
+    };
+  }
 
-    function onDragEnd() {
-      for (var i = 0; i < cells.length; i++) {
-        cells[i].style.outline = null;
+  function onDragEndFactory(els) {
+    return function () {
+      for (var i = 0; i < els.cells.length; i++) {
+        els.cells[i].style.outline = null;
       }
-    }
+    };
+  }
 
-    els.setupArtifacts.addEventListener('dragover', function (evt) {
-      evt.preventDefault();
-      return false;
-    });
+  function onDragOver(evt) {
+    evt.preventDefault();
+    return false;
+  }
 
-    els.setupArtifacts.addEventListener('drop', function (evt) {
+  function onDropFactory(els) {
+    return function (evt) {
       evt.preventDefault();
       if (!evt.target.classList.contains('setup-artifacts-cell')) {
         return;
       }
       evt.target.style.backgroundColor = null;
       if (!evt.target.hasChildNodes()) {
-        evt.target.appendChild(draggableEl.cloneNode(true));
+        evt.target.appendChild(els.draggable.cloneNode(true));
       }
-      for (var i = 0; i < cells.length; i++) {
-        cells[i].style.outline = null;
+      for (var i = 0; i < els.cells.length; i++) {
+        els.cells[i].style.outline = null;
       }
-    });
+    };
+  }
 
+  function onDragEnter(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('setup-artifacts-cell')) {
+      return;
+    }
+    // highlight a cell only if it's empty
+    if (!evt.target.hasChildNodes()) {
+      evt.target.style.backgroundColor = 'yellow';
+    }
+  }
 
-    els.setupArtifacts.addEventListener('dragenter', function (evt) {
-      evt.preventDefault();
-      if (!evt.target.classList.contains('setup-artifacts-cell')) {
-        return;
-      }
-      // highlight a cell only if it's empty
-      if (!evt.target.hasChildNodes()) {
-        evt.target.style.backgroundColor = 'yellow';
-      }
-    });
-
-    els.setupArtifacts.addEventListener('dragleave', function (evt) {
-      evt.preventDefault();
-      evt.target.style.backgroundColor = null;
-    });
-  };
+  function onDragLeave(evt) {
+    evt.preventDefault();
+    evt.target.style.backgroundColor = null;
+  }
 })();
